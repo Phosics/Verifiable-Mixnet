95467339f92dddc0a4065305d0f154a9d1e07d78
Guy Gal <galguy1@gmail.com>
2024-12-29 16:51:33 +0200

    Update PermutationNetwork.kt
    1) Handle validation
    2) Finish handling the mapping

--- VerifiableMixnet/src/main/kotlin/mixnet/PermutationNetwork.kt
+++ VerifiableMixnet/src/main/kotlin/mixnet/PermutationNetwork.kt
@@ -10,35 +10,45 @@ class PermutationNetwork(val n: Int) {
     private var bottom: PermutationNetwork? = null
 
     init {
+        require(n > 0 && (n and (n - 1)) == 0) { "n must be a power of 2." }
+
         if (n > 2) {
             top = PermutationNetwork(n / 2)
             bottom = PermutationNetwork(n / 2)
-            firstCol = MutableList(n / 2) { index ->
-                Switch()
-            }
-            lastCol = MutableList(n / 2) { index ->
-                Switch()
-            }
+            firstCol = MutableList(n / 2) { Switch() }
+            lastCol = MutableList(n / 2) { Switch() }
         } else {
             switch = Switch()
         }
     }
 
-    fun applyMap(votes: MutableList<Vote>): MutableList<Vote> {
-        val result: MutableList<Vote> = mutableListOf()
+    private fun applyFirstColMap(votes: MutableList<Vote>): List<Vote> {
+        val result: MutableList<Vote> = MutableList(votes.size) { votes[it] } // Initialize with same size
 
+        // Iterate over the first half of the input list
         for (i in 0 until votes.size / 2) {
-            result.add(i, votes[2 * i])
-            result.add(i + votes.size / 2, votes[2 * i + 1])
+            result[i] = votes[2 * i]                  // Add even-indexed element
+            result[i + votes.size / 2] = votes[2 * i + 1] // Add odd-indexed element
         }
 
         return result
     }
 
-    fun applyCol(votes: MutableList<Vote>, col: MutableList<Switch>): MutableList<Vote> {
+    private fun applyLastColMap(votes: MutableList<Vote>): MutableList<Vote> {
+        val result: MutableList<Vote> = MutableList(votes.size) { votes[it] } // Initialize with same size
+
+        for (i in 0 until votes.size / 2) {
+            result[2 * i] = votes[i]                  // Map first half to even indices
+            result[2 * i + 1] = votes[i + votes.size / 2] // Map second half to odd indices
+        }
+
+        return result
+    }
+
+    private fun applyCol(votes: MutableList<Vote>, col: MutableList<Switch>): MutableList<Vote> {
         val result: MutableList<Vote> = mutableListOf()
 
-        for (i in 0 until col.size) {
+        for (i in col.indices) {
             val subVotes = votes.subList(2 * i, 2 * i + 2)
             val appliedVote = col[i].apply(subVotes)
             result.addAll(appliedVote)
@@ -53,26 +63,23 @@ class PermutationNetwork(val n: Int) {
         }
 
         val firstColRes = firstCol?.let { applyCol(votes, it) }
+            ?: throw IllegalStateException("First column application failed.")
 
-        // TODO: add switching map
+        val firstColMappedVotes = applyFirstColMap(firstColRes).toMutableList()
 
-        if(firstColRes == null) {
-            // TODO: Handle error
-            return null
-        }
+        val topRes = top?.apply(firstColMappedVotes.subList(0, n / 2))
+            ?: throw IllegalStateException("Top network application failed.")
+        val bottomRes = bottom?.apply(firstColMappedVotes.subList(n / 2, n))
+            ?: throw IllegalStateException("Bottom network application failed.")
 
-        val topRes = top?.apply(firstColRes.subList(0, n / 2))
-        val bottomRes = bottom?.apply(firstColRes.subList(n / 2, n))
+        val combinedVotes = topRes + bottomRes
 
-        if(topRes == null || bottomRes == null) {
-            // TODO: Handle error
-            return null
-        }
+        val lastMapRes = applyLastColMap(combinedVotes.toMutableList())
 
-        topRes.addAll(bottomRes)
+        val lastColRes = lastCol?.let { applyCol(lastMapRes, it) }
+            ?: throw IllegalStateException("Last column application failed.")
 
-        // TODO: add switching map
-
-        return lastCol?.let { applyCol(topRes, it) }
+        return lastColRes
     }
+
 }
