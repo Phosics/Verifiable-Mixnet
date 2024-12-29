1bfdeecd8722a2a91776e9476f41c3be5a11515a
Guy Gal <galguy1@gmail.com>
2024-12-29 16:48:12 +0200

    Update switch
    some validations

--- VerifiableMixnet/src/main/kotlin/mixnet/Switch.kt
+++ VerifiableMixnet/src/main/kotlin/mixnet/Switch.kt
@@ -6,18 +6,25 @@ import java.util.function.Function
 class Switch : Function<MutableList<Vote>, MutableList<Vote>> {
     private var b = 1
 
+    // Set the switching flag
     fun setB(b: Int) {
+        require(b == 0 || b == 1) { "Switch value b must be 0 or 1." }
         this.b = b
     }
 
     override fun apply(votes: MutableList<Vote>): MutableList<Vote> {
-        // TODO: Add shuffle
-        // TODO: Validate both lists are in size 2
-        if (b == 1) {
-            votes.reverse()
-        }
+        // Validate size
+        require(votes.size == 2) { "Switch requires exactly 2 votes." }
 
-        return votes
-        // TODO: Add zero-knowledge proof
+        // Perform the switching operation
+        val result = if (b == 1) votes.reversed().toMutableList() else votes.toMutableList()
+
+        // TODO: Implement Zero-Knowledge Proof (ZKP) here
+
+        // TODO: Add the randomness before the return
+
+        return result
     }
+
+    // TODO: Add method to generate zero-knowledge proofs for correctness
 }
