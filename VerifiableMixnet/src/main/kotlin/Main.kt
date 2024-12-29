f7dda2cb52a9848d77891a28afb0095bf899aa2b
Guy Gal <galguy1@gmail.com>
2024-12-29 20:16:13 +0200

    Update Main.kt
    tests

--- VerifiableMixnet/src/main/kotlin/Main.kt
+++ VerifiableMixnet/src/main/kotlin/Main.kt
@@ -7,20 +7,32 @@ import java.math.BigInteger
 
 
 fun main() {
-    val mixServer = MixServer(4)
+    val mixServer = MixServer(8)
 
     val votes: MutableList<Vote> = mutableListOf(
         Vote(BigInteger.valueOf(0), BigInteger.valueOf(0), BigInteger.valueOf(128)),
         Vote(BigInteger.valueOf(1), BigInteger.valueOf(1), BigInteger.valueOf(128)),
         Vote(BigInteger.valueOf(2), BigInteger.valueOf(2), BigInteger.valueOf(128)),
-        Vote(BigInteger.valueOf(3), BigInteger.valueOf(3), BigInteger.valueOf(128))
+        Vote(BigInteger.valueOf(3), BigInteger.valueOf(3), BigInteger.valueOf(128)) ,
+        Vote(BigInteger.valueOf(4), BigInteger.valueOf(0), BigInteger.valueOf(128)),
+        Vote(BigInteger.valueOf(5), BigInteger.valueOf(1), BigInteger.valueOf(128)),
+        Vote(BigInteger.valueOf(6), BigInteger.valueOf(2), BigInteger.valueOf(128)),
+        Vote(BigInteger.valueOf(7), BigInteger.valueOf(3), BigInteger.valueOf(128))
     )
 
     println("Initial Votes:")
     votes.forEachIndexed { index, vote -> println("Vote $index: ${vote.getCipherText()}") }
 
-    val result = mixServer.apply(votes)
+    var result = mixServer.apply(votes)
+
+    println("\nFirst Shuffled Votes:")
+    result.forEachIndexed { index, vote -> println("Vote $index: ${vote.getCipherText()}") }
+
+    mixServer.configureRandomly()
+
+    result = mixServer.apply(votes)
 
     println("\nFinal Shuffled Votes:")
-    result?.forEachIndexed { index, vote -> println("Vote $index: ${vote.getCipherText()}") }
+    result.forEachIndexed { index, vote -> println("Vote $index: ${vote.getCipherText()}") }
+
 }
